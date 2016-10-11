4ebc2b19ccb702142402059eab824c782a8cc02c
Amy Eckert <amy@bluecadet.com>
2016-10-11 09:36:23 -0400

    working for submit, writes to json

--- lib/functions.php
+++ lib/functions.php
@@ -1,9 +1,28 @@
+<!--
+*******************************************************
+This is the home for reusable functions. Require this file on any page that needs to use any of its functions
+or pass a variable to/from.
+*******************************************************
+-->
+
 <?php 
 
     function get_pets() {
         $petsJson = file_get_contents('data/pets.json'); //brings contents of a json file into a variable. 
         $pets = json_decode($petsJson, true); //to convert json to an assoc array structure, correctly. 
+
         return $pets;
-    }  
+    } 
+
+    function save_pets($petsToSave) { //$petsToSave is defined on pets_new.php and passed here through the require statement on that file.
+    // echo '<pre>';
+    // var_dump($petsToSave);die;
+    // echo '</pre>';
+
+        $json = json_encode($petsToSave, JSON_PRETTY_PRINT); //encode the array as json, make it look nice
+        file_put_contents('data/pets.json', $json); //add it to the file.
+
+
+    }
   
 ?>
--- pets_new.php
+++ pets_new.php
@@ -1,36 +1,63 @@
 <?php require 'layout/header.php'; ?>
 <?php
+require 'lib/functions.php';
 
 // $name = $_POST['name'];
 // $breed = $_POST['breed'];
 // $weight = $_POST['weight'];
 // $bio = $_POST['bio'];
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-//  to make sure code doesn't break if form not filled out or it's missing:
-if (isset($_POST['name'])) {
-    $name=$_POST['name'];
-} else {
-    $name = '';
+    // echo '<pre>';
+    // var_dump($_SERVER);
+    // echo '</pre>';
+    //  to make sure code doesn't break if form not filled out or it's missing:
+    if (isset($_POST['name'])) {
+        $name=$_POST['name'];
+    } else {
+        $name = '';
+    }
+
+    if (isset($_POST['breed'])) {
+        $breed=$_POST['breed'];
+    } else {
+        $breed = '';
+    }
+
+    if (isset($_POST['weight'])) {
+        $weight=$_POST['weight'];
+    } else {
+        $weight = '';
+    }
+
+    if (isset($_POST['bio'])) {
+        $bio=$_POST['bio'];
+    } else {
+        $bio = '';
+    }
+    $pets = get_pets();
+
+    //to save a new pet to json, create an array using the properties needed. 
+    $newPet = array(
+        'name' => $name,
+        'breed' => $breed,
+        'weight' => $weight,
+        'bio' => $bio,
+        'age' => '', 
+        'image' => '',
+        );
+    $pets[] = $newPet; // add new pet to $pets array, without a specified. 
+
+    save_pets($pets);
+
+    // $json = json_encode($pets, JSON_PRETTY_PRINT); //encode the array as json, make it look nice
+    // file_put_contents('data/pets.json', $json); //add it to the file.
+
+    //redirect to home page after submission so additional submissins are unique: 
+    //ALWAYS DO THIS FOR A FORM SUBMIT!
+    header('Location: /'); //
+    die;
 }
-
-if (isset($_POST['breed'])) {
-    $name=$_POST['breed'];
-} else {
-    $name = '';
-}
-
-if (isset($_POST['weight'])) {
-    $name=$_POST['weight'];
-} else {
-    $name = '';
-}
-
-if (isset($_POST['bio'])) {
-    $name=$_POST['bio'];
-} else {
-    $name = '';
-}
-
 // echo '<pre>';
 // var_dump($name, $breed, $weight, $bio);die;
 // echo '</pre>';
